f26da4e1ff165ae440d967aa50c1840b0dd10843
varunbhandia <varunbhandia@gmail.com>
2020-06-06 22:54:02 +0530

    Finalised color and layout

--- application/views/Include/header.php
+++ application/views/Include/header.php
@@ -72,6 +72,25 @@
             </div>
         </nav>
         <div class="sidenav">
-            <a href="#about"><i class="fa fa-plus fa-1x" aria-hidden="true"></i></a>
+            <a href="" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-plus fa-1x" aria-hidden="true"></i></a>
+        </div>
+        <div class="modal fade " id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog " role="document">
+                <div class="modal-content dark-inner-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Import Data</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-dark">Import</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
